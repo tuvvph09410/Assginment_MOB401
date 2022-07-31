f94fd0368f81619da2ab63c3aad319dd38d59fc7
tuvvph09410 <tucaogia1@gmail.com>
2022-07-31 14:26:17 +0700

    upload fragment tietkiem

---
+++ app/src/main/java/com/example/assginment_mob403/Fragment/TietKiem/Fragment_tietkiem.java
@@ -0,0 +1,23 @@
+package com.example.assginment_mob403.Fragment.TietKiem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.assginment_mob403.R;
+
+
+public class Fragment_tietkiem extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tietkiem2, container, false);
+    }
+}
